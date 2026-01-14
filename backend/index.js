0419d87cf289b384873eefeb7b3a64b849f0e7c3
Christian <Christian.nicolau2312@gmail.com>
2026-01-14 16:24:28 +0100

    Multi-tier Docker app

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+const port = 5000;
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT NOW()");
+  res.json({
+    message: "Hello from Backend!",
+    time: result.rows[0],
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Backend running on port ${port}`);
+});
